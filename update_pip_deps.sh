6e9c78216022642bc832ddc1fff31527fbf4e477
Nils Tonnaett <ntonnatt@ccrma.stanford.edu>
2024-09-29 14:40:51 -0700

    update pip build dependencies

--- update_pip_deps.sh
+++ update_pip_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # https://github.com/flatpak/flatpak-builder-tools/tree/master/pip
-flatpak-pip-generator --runtime='org.freedesktop.Sdk//22.08' --requirements-file='requirements.txt' --output pypi-dependencies
+flatpak-pip-generator --runtime='org.freedesktop.Sdk//23.08' --requirements-file='requirements.txt' --output pypi-dependencies
